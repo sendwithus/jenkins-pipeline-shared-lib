2ea0503339975f7f7cfe3ac527850047284cf553
Matthieu Dolci <matthieu.dolci@gmail.com>
2019-05-06 14:27:25 -0700

    fix error

--- vars/skipStage.groovy
+++ vars/skipStage.groovy
@@ -9,7 +9,7 @@ def checkLint() {
     env.LINT_SKIP = "false"
     result = sh (script: "git log -1 | grep '.*\\[lint skip\\].*'", returnStatus: true)
     if (result == 0) {
-        env.CI_SKIP = "true"
+        env.LINT_SKIP = "true"
         error "'[lint skip]' found in git commit message. Aborting."
     }
 }
